5cbb24c8334819effc580c36e42d5905719371d7
Chris U <chris.uyehara@va.gov>
2012-11-07 09:06:06 -0800

    scheduling update

--- mdws/src/svc/SchedulingSvc.cs
+++ mdws/src/svc/SchedulingSvc.cs
@@ -2,12 +2,13 @@
 using System.ComponentModel;
 using gov.va.medora.mdws.dto;
 using System.ServiceModel;
+using System.ServiceModel.Web;
 using System.ServiceModel.Activation;
 
 namespace gov.va.medora.mdws
 {
     // WSE
-    [ServiceContract(Namespace = "http://mdws.medora.va.gov/SchedulingSvc")]
+    //[ServiceContract(Namespace = "http://mdws.medora.va.gov/SchedulingSvc")]
     [WebService(Namespace = "http://mdws.medora.va.gov/SchedulingSvc")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [ToolboxItem(false)]
@@ -16,37 +17,6 @@ namespace gov.va.medora.mdws
     public partial class SchedulingSvc : BaseService, ISchedulingSvc
     {
         public SchedulingSvc() : base() { }
-
-        [WebMethod(EnableSession = true, Description = "Get all VHA sites")]
-        public RegionArray getVHA()
-        {
-            return (RegionArray)MySession.execute("SitesLib", "getVHA", new object[] { });
-        }
-
-        [WebMethod(EnableSession = true, Description = "Connect to a single VistA system.")]
-        public DataSourceArray connect(string sitelist)
-        {
-            return (DataSourceArray)MySession.execute("ConnectionLib", "connectToLoginSite", new object[] { sitelist });
-        }
-
-        [WebMethod(EnableSession = true, Description = "Log onto a single VistA system.")]
-        public UserTO login(string username, string pwd, string context)
-        {
-            return (UserTO)MySession.execute("AccountLib", "login", new object[] { username, pwd, context });
-        }
-
-        [OperationContract]
-        [WebMethod(EnableSession = true, Description = "Disconnect from single Vista system.")]
-        public TaggedTextArray disconnect()
-        {
-            return (TaggedTextArray)MySession.execute("ConnectionLib", "disconnectAll", new object[] { });
-        }
-
-        [WebMethod(EnableSession = true, Description = "Select a patient at logged in site.")]
-        public PatientTO select(string DFN)
-        {
-            return (PatientTO)MySession.execute("PatientLib", "select", new object[] { DFN });
-        }
 
         [WebMethod(EnableSession = true, Description = "Get list of clinics")]
         public TaggedHospitalLocationArray getClinics(string target)
@@ -57,26 +27,37 @@ namespace gov.va.medora.mdws
         [WebMethod(EnableSession = true, Description = "Get clinic availability information")]
         public TextTO getClinicAvailability(string clinicId)
         {
-            return (TextTO)MySession.execute("EncounterLib", "getClinicAvailability", new object[] { clinicId });
+            return (TextTO)MySession.execute("SchedulingLib", "getClinicAvailability", new object[] { clinicId });
         }
 
         [WebMethod(EnableSession = true, Description = "Get clinic scheduling information. e.g. availability, default appointment length, etc.")]
         public HospitalLocationTO getClinicSchedulingDetails(string clinicId)
         {
-            //return new HospitalLocationTO() { fault = new FaultTO(new NotImplementedException()) };
-            return (HospitalLocationTO)MySession.execute("EncounterLib", "getClinicSchedulingDetails", new object[] { clinicId });
+            return (HospitalLocationTO)MySession.execute("SchedulingLib", "getClinicSchedulingDetails", new object[] { clinicId });
         }
 
         [WebMethod(EnableSession = true, Description = "Get list of appointment types")]
         public AppointmentTypeArray getAppointmentTypes(string target)
         {
-            return (AppointmentTypeArray)MySession.execute("EncounterLib", "getAppointmentTypes", new object[] { target });
+            return (AppointmentTypeArray)MySession.execute("SchedulingLib", "getAppointmentTypes", new object[] { target });
         }
 
         [WebMethod(EnableSession = true, Description = "Schedule an appointment")]
         public AppointmentTO makeAppointment(string clinicId, string appointmentType, string appointmentTimestamp, string appointmentLength)
         {
-            return (AppointmentTO)MySession.execute("EncounterLib", "makeAppointment", new object[] { clinicId, appointmentType, appointmentTimestamp, appointmentLength });
+            return (AppointmentTO)MySession.execute("SchedulingLib", "makeAppointment", new object[] { clinicId, appointmentType, appointmentTimestamp, appointmentLength });
+        }
+
+        [WebMethod(EnableSession = true, Description = "Get a patient's pending appointments")]
+        public TaggedAppointmentArray getPendingAppointments(string startDate)
+        {
+            return (TaggedAppointmentArray)MySession.execute("SchedulingLib", "getPendingAppointments", new object[] { startDate });
+        }
+
+        [WebMethod(EnableSession = true, Description = "Get a list of patients by clinic")]
+        public PatientArray getPatientsByClinic(string clinicId, string startDate, string stopDate)
+        {
+            return (PatientArray)MySession.execute("SchedulingLib", "getPatientsByClinic", new object[] { clinicId, startDate, stopDate });
         }
     }
 
@@ -85,15 +66,25 @@ namespace gov.va.medora.mdws
     public interface ISchedulingSvc
     {
         [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, UriTemplate = "clinics/{id}")]
         TaggedHospitalLocationArray getClinics(string target);
 
         [OperationContract]
         TextTO getClinicAvailability(string clinicId);
 
         [OperationContract]
-        AppointmentTO makeAppointment(string clinicId, string appointmentType, string appointmentTimestamp, string appointmentLength);
+        HospitalLocationTO getClinicSchedulingDetails(string clinicId);
 
         [OperationContract]
         AppointmentTypeArray getAppointmentTypes(string target);
+
+        [OperationContract]
+        AppointmentTO makeAppointment(string clinicId, string appointmentType, string appointmentTimestamp, string appointmentLength);
+
+        [OperationContract]
+        TaggedAppointmentArray getPendingAppointments(string startDate);
+
+        [OperationContract]
+        PatientArray getPatientsByClinic(string clinicId, string startDate, string stopDate);
     }
 }
